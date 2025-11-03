a3a160b108c7ffe7df7c65f8dba314c20557d32b
djoni1vincent <djoni8vincent@gmail.com>
2025-11-03 22:35:06 +0100

    make base for future programming, react + tailwind

---
+++ my-app/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+<>
+    <header className='text-6xl text-amber-600' >Impressive Books
+    </header>
+
+ 
+    </>
+  );
+}
+
+export default App;
+
